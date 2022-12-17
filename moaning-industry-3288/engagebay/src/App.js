427bc83f42d6eb0502ad16d5f9f85fc8da75dbea
Vaibhav <vaibhav.2071998@gmail.com>
2022-12-17 18:42:46 +0530

    added assest folder

--- moaning-industry-3288/engagebay/src/App.js
+++ moaning-industry-3288/engagebay/src/App.js
@@ -1,24 +1,24 @@
 
 
-import Plan from "./Components/Pricing/Plans/Month";
+// import Plan from "./Components/Pricing/Plans/Month";
 //import { BrowserRouter } from "react-router-dom";
 
-import Resource from './Components/Resource.jsx'
-import Head from './Components/Ram_HomePage/HeadPart/Head';
-import MiddleCenter from './Components/Ram_HomePage/MiddlePart/MiddleCenter';
-import MiddleUper from './Components/Ram_HomePage/MiddlePart/MiddleUper';
+// import Resource from './Components/Resource.jsx'
+// import Head from './Components/Ram_HomePage/HeadPart/Head';
+// import MiddleCenter from './Components/Ram_HomePage/MiddlePart/MiddleCenter';
+// import MiddleUper from './Components/Ram_HomePage/MiddlePart/MiddleUper';
 import { AutoDailer } from "./Components/AutoDailer";
-import React from 'react'
-import Om from "./Components/Pricing/Om";
+// import React from 'react'
+// import Om from "./Components/Pricing/Om";
 
 const App = () => {
   return (
     <div className="App">
-      <Head/>
+      {/* <Head/>
       <MiddleUper/>
       <MiddleCenter/>
       <Om/>
-      <Resource/>
+      <Resource/> */}
       <AutoDailer/>
    </div>
 
