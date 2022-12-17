4e8b3d18cb60232636b5e15d90b96aa1aaa4a339
Ram Naphade <105856357+ramNgithub@users.noreply.github.com>
2022-12-18 02:59:46 +0530

    ram-day-5

--- moaning-industry-3288/engagebay/src/App.js
+++ moaning-industry-3288/engagebay/src/App.js
@@ -6,27 +6,20 @@
 
 //import { BrowserRouter } from "react-router-dom";
 import Resource from './Components/Resource.jsx'
-import Head from './Components/Ram_HomePage/HeadPart/Head';
-import MiddleCenter from './Components/Ram_HomePage/MiddlePart/MiddleCenter';
-import MiddleUper from './Components/Ram_HomePage/MiddlePart/MiddleUper';
+
 import { AutoDailer } from "./Components/AutoDailer";
 import React from 'react'
 import Om from "./Components/Pricing/Om";
-import Footer from "./Components/Ram_HomePage/Footer/Footer"
 import Home from './Components/Ram_HomePage/Home.jsx';
 
 const App = () => {
   return (
     <div className="App">
-      {/* <Head/>
-      <MiddleUper/>
-      <MiddleCenter/>
       
-      <Footer/> */}
       <Home/>
-      {/* <Om/>
+      <Om/>
       <Resource/>
-      <AutoDailer/> */}
+      <AutoDailer/>
    </div>
 
   );
