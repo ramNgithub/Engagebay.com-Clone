d0a88e31d8ee92822ea808811e6351ea002109de
meharwan singh <rathode689@rediffmail.com>
2022-12-18 20:37:16 +0530

    m

--- moaning-industry-3288/engagebay/src/index.js
+++ moaning-industry-3288/engagebay/src/index.js
@@ -3,18 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
- import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+     <Provider>
+          <ChakraProvider>
+               <BrowserRouter>
+                    <App />
+               </BrowserRouter>
+          </ChakraProvider>
+     </Provider>
 
- 
-     <BrowserRouter>
-      <App />
-     </BrowserRouter>
-  
 
- 
+
 
 );
 
